d0bcbd26777524a769452c66c75a50d8098061ef
m.alejandrasrz <m.alejandrasrz@gmail.com>
2025-03-12 20:35:13 -0500

    feat - Estructuración de carpetas y configuración de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: '', 
+        loadChildren: () => import('./modules/tasks/tasks.module').then(m => m.TasksModule)
+      }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
